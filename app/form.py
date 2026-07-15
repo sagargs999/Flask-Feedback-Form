8854765f823fa3b343e9a237ac7ead883fb2d36f
Sagar <gs2112006@gmail.com>
2026-07-15 21:53:11 +0530

    Add files via upload

---
+++ app/form.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder=str(Path(__file__).resolve().parent.parent / "templates"))
+@app.route("/feedback", methods=["GET", "POST"])
+def feedback():
+    if request.method =="POST":
+        name = request.form.get("username")
+        message = request.form.get("message")
+        return render_template("thank_you.html", name=name, message=message)
+    return render_template("feedback.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
